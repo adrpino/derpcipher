d6a04513fc5e6f3ec1ba693cae47234f39c53c7c
adrian <adrianpinoalcalde@gmail.com>
2017-11-29 08:41:59 +0100

    password as string to function argument

--- main.go
+++ main.go
@@ -12,18 +12,17 @@ import (
 )
 
 // encrypts byte slice with a pass
-func encrypt(plaintext []byte) ([]byte, error) {
+func encrypt(plainText []byte, pass string) ([]byte, error) {
 	// Generate random salt for KDF
 	var salt = make([]byte, 8)
 	if _, err := io.ReadFull(rand.Reader, salt[:]); err != nil {
 		panic(err)
 	}
 	// Derive key with scrypt
-	secretKeyBytes, err := scrypt.Key([]byte("some password"), salt, 1<<15, 8, 1, 32)
+	secretKeyBytes, err := scrypt.Key([]byte(pass), salt, 1<<15, 8, 1, 32)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(len(secretKeyBytes))
 	var secretKey [32]byte
 	copy(secretKey[:], secretKeyBytes)
 	// encode it to hex
@@ -34,18 +33,18 @@ func encrypt(plaintext []byte) ([]byte, error) {
 	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
 		panic(err)
 	}
-	encrypted := secretbox.Seal(nonce[:], plaintext, &nonce, &secretKey)
+	encrypted := secretbox.Seal(nonce[:], plainText, &nonce, &secretKey)
 	// Add the salt at the beginning of the message:
 	encrypted = append(salt, encrypted...)
 	return encrypted, nil
 }
 
 // Decrypts a slice of bytes
-func decrypt(cipherText []byte) ([]byte, error) {
+func decrypt(cipherText []byte, pass string) ([]byte, error) {
 	// First 8 bytes of cyphertext is salt
 	var msgSalt = make([]byte, 8)
 	copy(msgSalt[:], cipherText[:8])
-	secretKeyBytes, err := scrypt.Key([]byte("some password"), msgSalt, 1<<15, 8, 1, 32)
+	secretKeyBytes, err := scrypt.Key([]byte(pass), msgSalt, 1<<15, 8, 1, 32)
 	if err != nil {
 		panic(err)
 	}
@@ -65,12 +64,12 @@ func main() {
 	fmt.Print("Enter text: ")
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	encrypted, err := encrypt([]byte(text))
-	fmt.Println("encrypted", string(encrypted))
+	encrypted, err := encrypt([]byte(text), "some password")
+	fmt.Println("Ciphertext:", string(encrypted))
 
-	decrypted, err := decrypt(encrypted)
+	decrypted, err := decrypt(encrypted, "some password")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(decrypted))
+	fmt.Println("Plaintext:", string(decrypted))
 }
